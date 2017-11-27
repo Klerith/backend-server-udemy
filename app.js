e72638aeac4a529050818b579be8544f29789790
Fernando <fernando.herrera85@gmail.com>
2017-11-27 16:09:58 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+// Inicializar variables
+var app = express();
+
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+});
+
+
+// Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+});
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
