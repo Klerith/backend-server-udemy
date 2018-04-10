f0f89b6ed766f7805bc1dda9c5cd6d5ba848afe8
Fernando <fernando.herrera85@gmail.com>
2018-04-10 17:32:58 -0600

    Actualización - nueva versión de Google SingIn
    # Actualización debido a los cambios del Google Sign-In en el backend
    
    Uso de Async y Await

--- routes/login.js
+++ routes/login.js
@@ -8,17 +8,16 @@ var app = express();
 var Usuario = require('../models/usuario');
 
 
-var GoogleAuth = require('google-auth-library');
-var auth = new GoogleAuth;
-
-const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
-const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+// Google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
 
 var mdAutenticacion = require('../middlewares/autenticacion');
 
 // ==========================================
-//  Autenticación De Google
+//  Renovar Token
 // ==========================================
 app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
 
@@ -31,114 +30,108 @@ app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
 
 });
 
-// ==========================================
-//  Autenticación De Google
-// ==========================================
-app.post('/google', (req, res) => {
-
-    var token = req.body.token || 'XXX';
-
-
-    var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
-
-    client.verifyIdToken(
-        token,
-        GOOGLE_CLIENT_ID,
+// =================================================
+//  Autenticación de Google
+// =================================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-        function(e, login) {
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
 
-            if (e) {
-                return res.status(400).json({
-                    ok: true,
-                    mensaje: 'Token no válido',
-                    errors: e
-                });
-            }
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
 
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
 
-            var payload = login.getPayload();
-            var userid = payload['sub'];
-            // If request specified a G Suite domain:
-            //var domain = payload['hd'];
+app.post('/google', async(req, res) => {
 
-            Usuario.findOne({ email: payload.email }, (err, usuario) => {
+    var token = req.body.token;
 
-                if (err) {
-                    return res.status(500).json({
-                        ok: true,
-                        mensaje: 'Error al buscar usuario - login',
-                        errors: err
-                    });
-                }
-
-                if (usuario) {
-
-                    if (usuario.google === false) {
-                        return res.status(400).json({
-                            ok: true,
-                            mensaje: 'Debe de usar su autenticación normal'
-                        });
-                    } else {
-
-                        usuario.password = ':)';
-
-                        var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }); // 4 horas
-
-                        res.status(200).json({
-                            ok: true,
-                            usuario: usuario,
-                            token: token,
-                            id: usuario._id,
-                            menu: obtenerMenu(usuario.role)
-                        });
-
-                    }
-
-                    // Si el usuario no existe por correo
-                } else {
-
-                    var usuario = new Usuario();
-
-
-                    usuario.nombre = payload.name;
-                    usuario.email = payload.email;
-                    usuario.password = ':)';
-                    usuario.img = payload.picture;
-                    usuario.google = true;
-
-                    usuario.save((err, usuarioDB) => {
-
-                        if (err) {
-                            return res.status(500).json({
-                                ok: true,
-                                mensaje: 'Error al crear usuario - google',
-                                errors: err
-                            });
-                        }
-
-
-                        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
-
-                        res.status(200).json({
-                            ok: true,
-                            usuario: usuarioDB,
-                            token: token,
-                            id: usuarioDB._id,
-                            menu: obtenerMenu(usuarioDB.role)
-                        });
-
-                    });
-
-                }
-
-
+    var googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no válido'
             });
-
-
         });
 
 
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usar su autenticación normal'
+                });
+            } else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            }
+
+        } else {
+            // El usuario no existe... hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+
+            usuario.save((err, usuarioDB) => {
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+            });
+
+        }
+
+
+    });
+
+
+
+
+    // return res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'OK!!!',
+    //     googleUser: googleUser
+    // });
 
 
 });
