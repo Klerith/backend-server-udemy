863628e00a3004d45b7b1bf49005c85d9fb55b37
Fernando <fernando.herrera85@gmail.com>
2018-04-10 18:59:54 -0600

    Actualización debido a depreciación de sendfile
    Usamos PATH y sendFile

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express();
+
+const path = require('path');
+const fs = require('fs');
 
 
 app.get('/:tipo/:img', (req, res, next) => {
@@ -9,18 +11,14 @@ app.get('/:tipo/:img', (req, res, next) => {
     var tipo = req.params.tipo;
     var img = req.params.img;
 
-    var path = `./uploads/${ tipo }/${ img }`;
+    var pathImagen = path.resolve(__dirname, `../uploads/${ tipo }/${ img }`);
 
-    fs.exists(path, existe => {
-
-        if (!existe) {
-            path = './assets/no-img.jpg';
-        }
-
-
-        res.sendfile(path);
-
-    });
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        var pathNoImagen = path.resolve(__dirname, '../assets/no-img.jpg');
+        res.sendFile(pathNoImagen);
+    }
 
 
 });
